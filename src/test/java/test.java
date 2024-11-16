3b23d76c8de7f2181d4fa4c77d677ac7e2b17171
Beginer543 <sobkovychv@gmail.com>
2024-11-15 22:44:01 -0500

    first commit

---
+++ src/test/java/test.java
@@ -0,0 +1,7 @@
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("this is the project 4 ");
+    }
+}
+//done with version three
